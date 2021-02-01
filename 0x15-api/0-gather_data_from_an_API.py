01e0e38929eaca1e4e5fac0b9a8de1c06e1d70e4
dreeseh <1859@holbertonschool.com>
2021-02-01 17:29:46 +0000

    project with task one

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""using given REST API:
+https://jsonplaceholder.typicode.com/
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+
+employee_id = argv[1]
+
+users_data = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}'.
+    format(employee_id))
+
+todo_list = requests.get(
+    'https://jsonplaceholder.typicode.com/todos?userId={}'.
+    format(employee_id))
+
+completed_tasks = 0
+
+dict_users = users_data.json()
+dict_todos = todo_list.json()
+employee_name = dict_users['name']
+total_tasks = 0
+
+for done_tasks in dict_todos:
+    if done_tasks.get('completed') is True:
+        completed_tasks += 1
+    total_tasks += 1
+
+print("Employee {} is done with tasks({}/{})):".format(
+       employee_name,
+       completed_tasks,
+       total_tasks))
+
+for done_tasks in dict_todos:
+    if done_tasks.get('completed') is True:
+        print("\t {}".format(done_tasks.get('title')))
