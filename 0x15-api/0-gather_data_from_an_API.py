1c35ff354162bcdb3ad8c569292f479063522c93
dreeseh <1859@holbertonschool.com>
2021-02-01 23:06:52 +0000

    indent makes it print once

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,15 +9,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    employee_id = argv[1]
-
     users_data = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.
-        format(employee_id))
+        format(argv[1]))
 
     todo_list = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.
-        format(employee_id))
+        format(argv[1]))
 
     dict_users = users_data.json()
     dict_todos = todo_list.json()
@@ -30,11 +28,11 @@ if __name__ == "__main__":
             completed_tasks += 1
         total_tasks += 1
 
-        print("Employee {} is done with tasks({}/{})):".format(
-            employee_name,
-            completed_tasks,
-            total_tasks))
+    print("Employee {} is done with tasks({}/{})):".format(
+        employee_name,
+        completed_tasks,
+        total_tasks))
 
-        for done_tasks in dict_todos:
-            if done_tasks.get('completed') is True:
-                print("\t {}".format(done_tasks.get('title')))
+    for done_tasks in dict_todos:
+        if done_tasks.get('completed') is True:
+            print("\t {}".format(done_tasks.get('title')))
