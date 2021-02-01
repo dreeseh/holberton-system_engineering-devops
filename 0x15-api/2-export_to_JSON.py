34bc6271e8a8ab219cfe6fae3421f15ac973fc08
dreeseh <1859@holbertonschool.com>
2021-02-01 23:32:30 +0000

    added __name__

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,28 +7,29 @@ import requests
 from sys import argv
 
 
-users_data = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.
-    format(argv[1])).json()
+if __name__ == "__main__":
+    users_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(argv[1])).json()
 
-todo_list = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.
-    format(argv[1])).json()
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(argv[1])).json()
 
 
-employee_name = users_data['name']
-completed_tasks = 0
-total_tasks = 0
+    employee_name = users_data['name']
+    completed_tasks = 0
+    total_tasks = 0
 
-todos = []
-for key in todo_list:
-    todo = {}
-    todo['task'] = key.get('title')
-    todo['completed'] = key.get('completed')
-    todo['username'] = users_data.get('username')
-    todos.append(todo)
+    todos = []
+    for key in todo_list:
+        todo = {}
+        todo['task'] = key.get('title')
+        todo['completed'] = key.get('completed')
+        todo['username'] = users_data.get('username')
+        todos.append(todo)
 
-dictionary = {argv[1]: todos}
+        dictionary = {argv[1]: todos}
 
-with open('{}.json'.format(argv[1]), mode='w') as jsonfile:
-    json.dump(dictionary, jsonfile)
+        with open('{}.json'.format(argv[1]), mode='w') as jsonfile:
+            json.dump(dictionary, jsonfile)
