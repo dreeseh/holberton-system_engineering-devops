87d96761e5c5bcf22710fe03bd1cc0d4b695a90e
dreeseh <1859@holbertonschool.com>
2021-02-01 22:58:04 +0000

    added if __name__

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,32 +8,33 @@ from sys import argv
 import requests
 
 
-employee_id = argv[1]
+if __name__ == "__main__":
+    employee_id = argv[1]
 
-users_data = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.
-    format(employee_id))
+    users_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(employee_id))
 
-todo_list = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.
-    format(employee_id))
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(employee_id))
 
-dict_users = users_data.json()
-dict_todos = todo_list.json()
-employee_name = dict_users['name']
-completed_tasks = 0
-total_tasks = 0
+    dict_users = users_data.json()
+    dict_todos = todo_list.json()
+    employee_name = dict_users['name']
+    completed_tasks = 0
+    total_tasks = 0
 
-for done_tasks in dict_todos:
-    if done_tasks.get('completed') is True:
-        completed_tasks += 1
-    total_tasks += 1
+    for done_tasks in dict_todos:
+        if done_tasks.get('completed') is True:
+            completed_tasks += 1
+        total_tasks += 1
 
-print("Employee {} is done with tasks({}/{})):".format(
-       employee_name,
-       completed_tasks,
-       total_tasks))
+        print("Employee {} is done with tasks({}/{})):".format(
+            employee_name,
+            completed_tasks,
+            total_tasks))
 
-for done_tasks in dict_todos:
-    if done_tasks.get('completed') is True:
-        print("\t {}".format(done_tasks.get('title')))
+        for done_tasks in dict_todos:
+            if done_tasks.get('completed') is True:
+                print("\t {}".format(done_tasks.get('title')))
