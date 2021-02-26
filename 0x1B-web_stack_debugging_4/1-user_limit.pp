a7329b99e5bc2d7f53e834b6f3953177e61bef62
dreeseh <1859@holbertonschool.com>
2021-02-26 03:29:35 +0000

    took out extra lines of comment

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,6 @@
 # Change the OS configuration so that it is
-# possible to login with the holberton user
-# and open a file without any error message
 
-exec {'increase OS Limits':
+exec {'increase OS limits':
      command => 'sudo sed -i "s/5/3000/g; s/4/3000/g" /etc/security/limits.conf',
      path    => '/bin:/usr/bin:/usr/sbin:/sbin',
 }
