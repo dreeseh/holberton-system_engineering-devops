e3454224c93e0421de905f0107a4fa3c07531074
dreeseh <1859@holbertonschool.com>
2021-02-26 03:34:43 +0000

    linter fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
 # Change the OS configuration so that it is
 
 exec {'increase OS limits':
-     command => "sed -i 's/worker_processes 4;/worker_processes 8;/g'
-     /etc/security/limits.conf; sudo service nginx restart",
-     path    => ['/bin', '/usr/bin']
+  command => "sed -i 's/worker_processes 4;/worker_processes 8;/g' /etc/security/limits.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin']
 }
