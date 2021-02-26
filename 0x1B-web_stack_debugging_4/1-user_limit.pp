e7df7ec40c1f915abe62cddbec6ac76d3e3243c6
dreeseh <1859@holbertonschool.com>
2021-02-26 03:28:01 +0000

    try 2

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,8 @@
-# Change the holberton's user limit of open files.
+# Change the OS configuration so that it is
+# possible to login with the holberton user
+# and open a file without any error message
 
-$upper_limit='holberton hard nofile [0-9]*'
-$lower_limit='holberton soft nofile [0-9]*'
-$desired_upper_limit='holberton hard nofile 50000'
-$desired_lower_limit='holberton soft nofile 30000'
-$set_upper="'s/${upper_limit}/${desired_upper_limit}/g'"
-$set_lower="'s/${lower_limit}/${desired_lower_limit}/g'"
-$conf_file='limits.conf'
-$change_limits="sudo sed -i -e ${set_upper} -e ${set_lower} ${conf_file}"
-
-exec {'Increase holberton user limit of open files':
-    command => $change_limits,
-    path    => '/usr/bin',
-    cwd     => '/etc/security'
+exec {'increase OS Limits':
+     command => 'sudo sed -i "s/5/3000/g; s/4/3000/g" /etc/security/limits.conf',
+     path    => '/bin:/usr/bin:/usr/sbin:/sbin',
+}
